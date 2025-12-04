54d3505241d82b7bf0fd04d705c556b94708410c
Lorenzo Manacorda <lorenzo@mailbox.org>
2025-12-04 15:46:51 +0100

    ipallocator: remove altda prefix

--- pkg/ipallocator/constants.go
+++ pkg/ipallocator/constants.go
@@ -1,8 +1,5 @@
 package ipallocator
 
-// ServiceName is the name of the IPAllocator service in Restate
-const ServiceName = "AltDAIPAllocator"
-
 // Method names for the IPAllocator service
 const (
 	MethodAllocateNetworkBlock      = "AllocateNetworkBlock"
